bd17c15ebada447d804c141d9059e42b451eadf6
Khurshid Alam <khurshid1556@gmail.com>
2024-10-12 18:41:42 +0530

    loder is complited 90%

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-function loder(params) {}
+const h2 = document.querySelector("#line_part_1 h2");
+
+let tl = gsap.timeline();
+
+tl.to("#loder .line h3", {
+  opacity: 1,
+  duration: 0.3,
+  onStart: function () {
+    let count = "";
+    setInterval(function () {
+      if (count === 100) {
+        clearInterval();
+      } else {
+        count++;
+        h2.innerHTML = count;
+      }
+    }, 30);
+  },
+});
+
+tl.from("#loder .line h1, .line p", {
+  y: 160,
+  duration: 0.6,
+  stagger: 0.5,
+  delay: 0.5,
+});
+tl.to("#loder", {
+  opacity: 0,
+  duration: 0.7,
+  delay: 1.5,
+  displaly: "none",
+});
 
 function crsrAnamation() {
   document.addEventListener("mousemove", function (e) {
